442d3d52df79735fcd2308c366bde9f7bdab696f
skliba <kliba.stefano@gmail.com>
2016-01-06 17:55:53 +0100

    setup

--- app/src/main/java/hr/foi/mjurinic/postter/activities/MainActivity.java
+++ app/src/main/java/hr/foi/mjurinic/postter/activities/MainActivity.java
@@ -1,15 +1,35 @@
 package hr.foi.mjurinic.postter.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import hr.foi.mjurinic.postter.R;
 
 public class MainActivity extends BaseActivity {
+
+    @Bind(R.id.main_tab_layout)
+    TabLayout mainTabLayout;
+
+    @Bind(R.id.main_view_pager)
+    ViewPager mainViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initTabs();
+
+
     }
+
+    private void initTabs() {
+
+    }
+
 }
+
